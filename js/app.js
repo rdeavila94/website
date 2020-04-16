be3525d3d8d0cb6478df0134d25438d56a3576b7
rdeavila <richard.deavila@mobileforming.com>
2020-04-15 23:44:02 -0700

    Have a basic working terminal text animation as the header

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('ready to go!');
+const $terminal = document.querySelector('#terminal');
+
+const richardDeAvila = 'Richard De Avila';
+
+function textAnimation(el, content, speed=100) {
+  const recursiveAnimate = (element, content, position) => {
+    if (position < content.length) {
+      element.textContent = element.textContent + content.charAt(position);
+      setTimeout(() => recursiveAnimate(element, content, ++position), speed);
+    }
+  };
+
+  recursiveAnimate(el, content, 0);
+}
+
+textAnimation(terminal, richardDeAvila);
