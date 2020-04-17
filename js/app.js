59c8581f7343b77e9add5df30dee02d2d9d44c2b
rdeavila <richard.deavila@mobileforming.com>
2020-04-16 19:11:25 -0700

    Finalized the terminal animation, which will smooth scroll down into the body

--- js/app.js
+++ js/app.js
@@ -1,16 +1,43 @@
 const $terminal = document.querySelector('#terminal');
+const $body = document.querySelector('#body');
 
-const richardDeAvila = 'Richard De Avila';
+const richardDeAvila = `start "Richard De Avila"`;
 
-function textAnimation(el, content, speed=100) {
-  const recursiveAnimate = (element, content, position) => {
+function textAnimation({ el, content, speed = 100 }, callback) {
+  const recursiveAnimate = position => {
     if (position < content.length) {
-      element.textContent = element.textContent + content.charAt(position);
-      setTimeout(() => recursiveAnimate(element, content, ++position), speed);
+      el.textContent = el.textContent + content.charAt(position);
+      setTimeout(() => recursiveAnimate(++position), speed);
+    } else {
+      callback();
     }
   };
 
-  recursiveAnimate(el, content, 0);
+  recursiveAnimate(0);
 }
 
-setTimeout(() => textAnimation(terminal, richardDeAvila), 500);
+let counter = 1;
+
+function smoothScroll($destination) {
+  console.log('inside');
+  if (window.pageYOffset >= $destination.offsetHeight) {
+    return;
+  }
+  window.scrollTo(0, window.pageYOffset + counter++ / 10);
+  setTimeout(() => smoothScroll($destination), 1);
+}
+
+function pressEnter($line) {
+  $line.insertAdjacentHTML('afterend', `<br><span>>&nbsp;&nbsp;</span>`);
+}
+
+setTimeout(
+  () =>
+    textAnimation({ el: $terminal, content: richardDeAvila, speed: 50 }, () =>
+      setTimeout(() => {
+        pressEnter($terminal);
+        setTimeout(() => smoothScroll($body), 250);
+      }, 500)
+    ),
+  500
+);
